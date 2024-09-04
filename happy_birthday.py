ef8951010a3a3a3c3f43df7d44138c984c5d5080
zamannnn786 <zk4508@g.rit.edu>
2024-09-04 15:03:24 +0400

    version1

--- happy_birthday.py
+++ happy_birthday.py
@@ -1,6 +1,13 @@
-name = input("Please state your name.")
-birth_month = input("Please state your birth month")
-birth_day = input("Please state the birth day of the the month")
-birth_year = input("Please state your birth year")
+def bday_message():
+    name0 = input("Please state your name.")
+    birth_month0 = input("Please state your birth month")
+    birth_day0 = input("Please state the birth day of the the month")
+    birth_year0 = input("Please state your birth year")
 
-print(name, "your birthday is on", birth_month, birth_day,",", birth_year)
+    print(name0, "your birthday is on", birth_month0, birth_day0,",", birth_year0)
+
+    
+for i in range(0,3):
+    bday_message()
+    
+
