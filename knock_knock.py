5c2b3f86ae6639dde2695fcff25be8b03fe8a356
zamannnn786 <zk4508@g.rit.edu>
2024-09-11 14:24:34 +0400

    version 3

--- knock_knock.py
+++ knock_knock.py
@@ -1,5 +1,5 @@
 def joke():
-    knockername = input("Knock-Knock: Who's there?")
+    knockername = input("Knock-Knock: Who's there?")    
     print(knockername, "who?")
     print("I didn't know you could yodel!")
 
