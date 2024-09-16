6622396a3a5928c60a75363db58bc281bcf729aa
zamannnn786 <zk4508@g.rit.edu>
2024-09-16 15:42:44 +0400

    version 2

--- test_file.py
+++ test_file.py
@@ -1,7 +1,12 @@
 
 from samplefile import add
+from samplefile import multiply
 
 def test_add():
     assert add(2,3) == 5
     assert add(-1, 1) == 0
     assert add(92,8) == 100
+
+def test_multiply():
+    assert multiply(2,3) == 6
+    assert multiply(0,0) == 0
